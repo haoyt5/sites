02b4c92f163278294f76c47fc74e599d9d7a20db
hy2u <karenhaoyitu@gmail.com>
2018-07-01 22:59:35 -0700

    index

--- p2-haoyt5-master/t2/views.py
+++ p2-haoyt5-master/t2/views.py
@@ -3,3 +3,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 't2/index.html')
+
+def first(request):
+    return render(request, 'static/t2/1.png')
+
+def second(request):
+    return render(request, 'static/t2/2.png')
+
+def third(request):
+    return render(request, 'static/t2/3.png')
+
+def fourth(request):
+    return render(request, 'static/t2/4.png')
+
+def fifth(request):
+    return render(request, 'static/t2/5.png')
