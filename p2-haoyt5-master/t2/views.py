d0cf3874c576dc6785e8c7ddec1a1ba0743e80d7
hy2u <karenhaoyitu@gmail.com>
2018-07-02 04:04:48 -0700

    fix

---
+++ p2-haoyt5-master/t2/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 't2/index.html')
+  
+def one(request):
+    return render(request, 't2/1.html')
+
+def two(request):
+    return render(request, 't2/2.html')
+
+def three(request):
+    return render(request, 't2/3.html')
+
+def four(request):
+    return render(request, 't2/4.html')
+
+def five(request):
+    return render(request, 't2/5.html')
