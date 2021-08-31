fe86af912443dd95bb50c5cc57becc89a90cfcb4
Karen HY Tu <karenhaoyitu@gmail.com>
2021-08-31 21:38:09 +0800

    Configure the custom scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
 import "./index.css";
 import App from "./App";
 
