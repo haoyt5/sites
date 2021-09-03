1d3cb5f7addc0d7a011fd16240824ff40a70389e
Karen HY Tu <karenhaoyitu@gmail.com>
2021-09-04 00:35:40 +0800

    Disable the router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route component={Landing} />
-			</Switch>
-		</Router>
-	);
+	return <Landing />;
 }
 
 export default App;
