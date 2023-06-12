33fed41af22038de9767635dbf2d0ea0f8af97e9
Karen HY Tu <karenhaoyitu@gmail.com>
2023-06-12 15:59:34 -0700

    Updates

--- static/js/6.5fba180f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsites=this.webpackJsonpsites||[]).push([[6],{41:function(s,e,t){"use strict";t.r(e);t(2);var i=t(0);e.default=function(){return Object(i.jsx)("div",{style:{width:"200px"},children:Object(i.jsx)("img",{src:"/assets/img/karen_profile.png",className:"img-fluid rounded",alt:"..."})})}}}]);
-//# sourceMappingURL=6.5fba180f.chunk.js.map
